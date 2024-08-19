a3e2b8b8ecff854ee365be47709eb31ce83015da
Potapenko Vadim <vpotapenko@studio-tg.ru>
2024-08-19 13:48:40 +0300

    fix: update api route

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
   const configService = app.get(ConfigService);
   const appTitle = configService.get('APP_NAME');
   const version = configService.get('APP_VERSION');
